351745069d16524f5c82daaa2551f9ef69ca9d00
ngue-237 <ngueemmanuel.prof@gmail.com>
2025-02-01 11:08:30 +0100

    first commit

---
+++ src/main/java/com/logonedigital/pi_mentorat_virtuel/PiMentoratVirtuelApplication.java
@@ -0,0 +1,13 @@
+package com.logonedigital.pi_mentorat_virtuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiMentoratVirtuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiMentoratVirtuelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logonedigital/pi_mentorat_virtuel/PiMentoratVirtuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logonedigital.pi_mentorat_virtuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiMentoratVirtuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
