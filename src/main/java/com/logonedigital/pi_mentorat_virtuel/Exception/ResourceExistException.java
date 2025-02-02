f7c39604f37bb96791775d42c82d21ba20233123
Allaminehafiz <allaminhafiz68@gmail.com>
2025-02-02 11:38:48 +0100

    ajout

--- src/main/java/com/logonedigital/pi_mentorat_virtuel/Exception/ResourceExistException.java
+++
@@ -1,7 +0,0 @@
-package com.logonedigital.pi_mentorat_virtuel.Exception;
-
-public class ResourceExistException extends RuntimeException{
-    public ResourceExistException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/logonedigital/pi_mentorat_virtuel/dto/LocationRespDTO.java
+++
@@ -1,6 +0,0 @@
-package com.logonedigital.pi_mentorat_virtuel.dto;
-
-public record LocationRespDTO(String town,
-                              Integer postalCode,
-                              String street) {
-}
